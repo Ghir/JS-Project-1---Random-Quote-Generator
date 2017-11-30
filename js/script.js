5e6384fd3706f36ab193c60fe48b127a29747c28
Dario Ghirardo <dario.ghirardo@gmail.com>
2017-11-30 17:16:26 +0100

    Added description in script file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+/* TREEHOUSE TECHDEGREE FULL STACK JAVASCRIPT - PROJECT 1 - RANDOM QUOTE GENERATOR
+
+What the program does: 
+On button click, it displays a random quote to the user and changes the background to a new random RGB color. 
++ Category buttons can be used to filter the quotes. 
++ Quotes change automatically every 20 seconds 
++ Timer is reset every time the button is clicked. */
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 // call the function every 20 seconds
