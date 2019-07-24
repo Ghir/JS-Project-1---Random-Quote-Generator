71360133aa24c9ab7eb9df85ac7dfb5eb4941571
Dario <dario.ghirardo@gmail.com>
2019-07-24 20:47:58 +0200

    Refactored

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Displaying random quotes filterable by category.
+This simple app shows you random quotes filterable by category.
+
+&nbsp;
+&nbsp;
+&nbsp;
+
+![Screeshot](.quotes.png)
--- js/script.js
+++ js/script.js
@@ -1,67 +1,37 @@
-// print a new quote on button click and every 10 seconds
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-let intervalID = setInterval(printQuote, 10000);
+document.querySelector('#loadQuote').addEventListener("click", printQuote, false);
+let interval = setInterval(printQuote, 10000);
+const filterButtons = document.querySelectorAll('.filter');
 
-// toggle class selected on  filter buttons
-const filterButtons = document.getElementsByClassName('filter');
-for (let i=0; i<filterButtons.length; i++) {
-  filterButtons[i].addEventListener("click", function () {
-    if (filterButtons[i].classList.contains('selected')) {
-      filterButtons[i].classList.remove("selected")
-    }
-    else {
-      for (let i=0; i<filterButtons.length; i++) {
-        filterButtons[i].classList.remove("selected");
-      }
-      filterButtons[i].classList.add("selected");
-    }
-  })}
-
-function getRandomQuote () {
-  // return only Wisdom quotes
-  if (filterButtons[0].classList.contains('selected')) {
-    const randomNum = Math.floor(Math.random() * 10);
-    const wisdomArray = quotes.filter(function (el) {
-      return (el.tags === "Wisdom");
-    });
-    return wisdomArray[randomNum];
+filterButtons.forEach(button => button.addEventListener("click", () => {
+  if (button.classList.contains('selected')) {
+    button.classList.remove("selected");
   }
-  // return only Motivational quotes
-  else if (filterButtons[1].classList.contains('selected')) {
-    const randomNum = Math.floor(Math.random() * 10);
-    const motivationalArray = quotes.filter(function (el) {
-      return (el.tags === "Motivational");
-    });
-    return motivationalArray[randomNum];
-  }
-  // return only Humor quotes
-  else if (filterButtons[2].classList.contains('selected')) {
-    const randomNum = Math.floor(Math.random() * 10);
-    const humorArray = quotes.filter(function (el) {
-      return (el.tags === "Humor");
-    });
-    return humorArray[randomNum];
-  }
-  // return any quote
   else {
-    const randomNum = Math.floor(Math.random() * 30);
-    return quotes[randomNum]
+    filterButtons.forEach(button => button.classList.remove("selected"));
+    button.classList.add("selected");
   }
+}));
+
+function getRandomQuote ()  {
+  const selectedEl = document.querySelector('.selected');
+  if (selectedEl) {
+    const quotesArray = quotes.filter(quote => quote.tags === selectedEl.textContent);
+    return quotesArray[Math.floor(Math.random() * 10)];
+  }
+  return quotes[Math.floor(Math.random() * 30)];
 }
 
 function printQuote () {
-  const quoteObject = getRandomQuote();
+  const el = getRandomQuote();
   const paragraph = `
-  <p class="quote"> ${quoteObject.quote} </p>
-  <p class="source">  ${quoteObject.source} </p>
+  <p class="quote"> ${el.quote} </p>
+  <p class="source">  ${el.source} </p>
   `;
-  document.getElementById('quote-box').innerHTML = paragraph;
-  // set random background
+  document.querySelector('#quote-box').innerHTML = paragraph;
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
-  document.getElementsByTagName('body')[0].style.backgroundColor = `rgb(${red},${green},${blue})`;
-  clearInterval(intervalID);
-  intervalID = setInterval(printQuote, 20000);
+  document.querySelector('body').style.backgroundColor = `rgb(${red},${green},${blue})`;
+  clearInterval(interval);
+  interval = setInterval(printQuote, 20000);
 }
-
